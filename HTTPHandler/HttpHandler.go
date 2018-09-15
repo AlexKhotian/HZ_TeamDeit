6c1ecdd6e2290095692908103cfab835e00a74c5
AlexKhotian <doctaww@gmail.com>
2018-09-15 09:37:25 +0200

    simple ui

--- HTTPHandler/HttpHandler.go
+++ HTTPHandler/HttpHandler.go
@@ -22,21 +22,21 @@ func HTTPHandlerFactory() *HTTPHandlerUtil {
 func (handler *HTTPHandlerUtil) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
 	if r.Method == "GET" {
-		if r.URL.Path == "/Demands" {
+		if r.URL.Path == "/Ings" {
 			m, err := url.ParseQuery(r.URL.RawQuery)
 			if err != nil {
 				log.Println("Failed to parse url: ", m["h"][0])
 				return
 			} else {
 				log.Println("Requested day type: ", m["h"][0])
-				fragment, err := strconv.Atoi(m["h"][0])
+				_, err := strconv.Atoi(m["h"][0])
 				if err != nil {
 					log.Println("Failed to convert fragment")
 				}
 				w.Header().Set("Content-Type", "application/json")
 				w.Header().Set("Access-Control-Allow-Origin", "*")
 				w.Header().Set("Access-Control-Allow-Credentials", "true")
-				handler.HandleDemandsRequest(w, fragment)
+				//handler.HandleDemandsRequest(w, fragment)
 			}
 			return
 		}
@@ -60,7 +60,7 @@ func (handler *HTTPHandlerUtil) ServeHTTP(w http.ResponseWriter, r *http.Request
 
 func (handler *HTTPHandlerUtil) parseRequest(path *string) (string, string) {
 	var contentType string
-	modifiedSourcePath := "../../Webview" + *(path)
+	modifiedSourcePath := "Webview" + *(path)
 	if strings.HasSuffix(*path, ".html") {
 		contentType = "text/html"
 	} else if strings.HasSuffix(*path, ".css") {
