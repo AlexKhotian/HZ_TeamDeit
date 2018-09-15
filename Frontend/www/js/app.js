572052f86e0d26c7d2682aa9689524903160bc4d
AlexKhotian <doctaww@gmail.com>
2018-09-16 00:50:05 +0200

    fixed

--- Frontend/www/js/app.js
+++ Frontend/www/js/app.js
@@ -1,6 +1,7 @@
 // Dom7
 var $$ = Dom7;
 
+var poebota = [];
 // Framework7 App main instance
 var app = new Framework7({
     root: '#app', // App root element
@@ -31,6 +32,16 @@ var mainView = app.views.create('.view-main', {
     url: '/'
 });
 
-$$('#page-content .button-fill').on('click', function() {
-    app.dialog.alert('test');
+app.on('test3', function() {
+    // document.querySelector("")
+
+    /*   var formData = app.form.fillFromData('#thirdScreen', {
+           usuallyEat: "Bananas! 🍌"
+       });
+       var secondScreenFormData = app.form.convertToData('#secondScreen');
+       console.log("secondScreenFormData", secondScreenFormData)*/
+    var formData = app.form.convertToData('#thirdScreen');
+    app.request.get('http://127.0.0.1:7777/Ings?h=' + formData.usuallyEat, function(data) {
+        console.log(data);
+    });
 });
--- Frontend/www/js/connector.js
+++ Frontend/www/js/connector.js
@@ -1,16 +1,21 @@
-var $$ = Dom7;
-$$('.mealtime_test .submit_ing_btn2').on('click', function(e) {
-    console.log("test");
+$(document).ready(function() {
+    var myApp = new Framework7();
+    var $$ = Dom7;
+
+    myApp.on('test3', function() {
+        console.log("test");
+        alert("test");
+    });
 });
 
-
 function LoadRecipe() {
-    const request = require('request');
+    /* const request = require('request');
 
-    request('http://127.0.0.1:7777/Ings?h=Beef', { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
-        console.log(body.url);
-        console.log(body.explanation);
-    });
+     request('http://127.0.0.1:7777/Ings?h=Beef', { json: true }, (err, res, body) => {
+         if (err) { return console.log(err); }
+         console.log(body.url);
+         console.log(body.explanation);
+     });*/
+    console.log("test");
+    alert("test");
 }
-$(document).ready(LoadRecipe)
